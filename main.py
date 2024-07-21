541ab7b54ef8914153d02514d936d8e472aec94c
Rudransh Pratap Singh <87255169+CoderFleet@users.noreply.github.com>
2024-07-21 21:04:48 +0530

    state, toggle, timer, logic
    & Guess what speed too

--- main.py
+++ main.py
@@ -12,12 +12,18 @@ class GameOfLife:
         self.grid_size = 50
         self.cell_size = 12
         self.grid = [[0] * self.grid_size for _ in range(self.grid_size)]
+        self.running = False
+        self.interval = 100  # milliseconds
 
         self.draw_grid()
 
         self.canvas.bind("<Button-1>", self.toggle_cell)
-        self.master.bind("<space>", self.start_game)
+        self.master.bind("<space>", self.toggle_game)
         self.master.bind("<Escape>", self.stop_game)
+        self.master.bind("<Up>", self.increase_speed)
+        self.master.bind("<Down>", self.decrease_speed)
+
+        self.update_timer = None
 
     def draw_grid(self):
         for i in range(self.grid_size):
@@ -35,11 +41,57 @@ class GameOfLife:
         self.grid[x][y] = 1 - self.grid[x][y]
         self.draw_grid()
 
+    def toggle_game(self, event):
+        if self.running:
+            self.stop_game(None)
+        else:
+            self.start_game(None)
+
     def start_game(self, event):
-        pass
+        self.running = True
+        self.update_grid()
+        self.update_timer = self.master.after(self.interval, self.update_grid)
 
     def stop_game(self, event):
-        pass
+        self.running = False
+        if self.update_timer:
+            self.master.after_cancel(self.update_timer)
+            self.update_timer = None
+
+    def update_grid(self):
+        if not self.running:
+            return
+
+        new_grid = [[0] * self.grid_size for _ in range(self.grid_size)]
+        for i in range(self.grid_size):
+            for j in range(self.grid_size):
+                neighbors = sum(
+                    self.grid[x][y]
+                    for x in range(max(0, i-1), min(self.grid_size, i+2))
+                    for y in range(max(0, j-1), min(self.grid_size, j+2))
+                    if (x, y) != (i, j)
+                )
+                if self.grid[i][j] == 1 and neighbors in [2, 3]:
+                    new_grid[i][j] = 1
+                elif self.grid[i][j] == 0 and neighbors == 3:
+                    new_grid[i][j] = 1
+
+        self.grid = new_grid
+        self.draw_grid()
+        self.update_timer = self.master.after(self.interval, self.update_grid)
+
+    def increase_speed(self, event):
+        if self.interval > 10:
+            self.interval -= 10
+            if self.running:
+                self.stop_game(None)
+                self.start_game(None)
+
+    def decrease_speed(self, event):
+        self.interval += 10
+        if self.running:
+            self.stop_game(None)
+            self.start_game(None)
 
 if __name__ == "__main__":
     root = tk.Tk()
